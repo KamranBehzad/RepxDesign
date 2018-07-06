67b83f1ee741868d0d0269aac9f6c782d0181550
Kamran.Behzad <kamran.behzad@oohmedia.com.au>
2018-07-06 11:56:31 +1000

    Add project files.

---
+++ RepxDesigner/MainForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace RepxDesigner
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(12, 9);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(580, 25);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "Edit DevX Reports within Design Form opened on screen ...";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(504, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(342, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open New Design Form";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OpenDesignerClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 112);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RepX Designer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RepxDesigner/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.XtraReports.UI;
+
+namespace RepxDesigner
+{
+    public partial class MainForm : Form
+    {
+        private ReportDesignTool _designer;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenDesigner();
+        }
+
+        private void OpenDesigner()
+        {
+            _designer = new ReportDesignTool(new XtraReport());
+            _designer.ShowRibbonDesigner();
+        }
+
+        private void OpenDesignerClick(object sender, EventArgs e)
+        {
+            OpenDesigner();
+        }
+    }
+}
